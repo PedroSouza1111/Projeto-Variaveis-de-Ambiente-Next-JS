33f7626b77f94febc92b7e8b085548d81013854e
Laboratório FIAP <labsfiap@fiap.com.br>
2024-10-28 22:25:15 -0300

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import {Loader} from "@googlemaps/js-api-loader"
+
+const loader = new Loader({
+  apiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_KEY,
+  version: "weekly",
+})
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Env vars</h1>
+    </main>
+  )
+}
